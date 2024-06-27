a069b510f40f26ec588e3861d41fc1c6b254b484
Mark Goadrich <goadrich@hendrix.edu>
2024-06-27 14:27:31 -0500

    Adding the broken app and tests

--- to_dont_list/lib/main.dart
+++ to_dont_list/lib/main.dart
@@ -1,20 +1,89 @@
+// Started with https://docs.flutter.dev/development/ui/widgets-intro
 import 'package:flutter/material.dart';
+import 'package:to_dont_list/objects/item.dart';
+import 'package:to_dont_list/widgets/to_do_items.dart';
+import 'package:to_dont_list/widgets/to_do_dialog.dart';
 
-void main() {
-  runApp(const MainApp());
+class ToDoList extends StatefulWidget {
+  const ToDoList({super.key});
+
+  @override
+  State createState() => _ToDoListState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _ToDoListState extends State<ToDoList> {
+  final List<Item> items = [const Item(name: "add more todos")];
+  final _itemSet = <Item>{};
+
+  void _handleListChanged(Item item, bool completed) {
+    setState(() {
+      // When a user changes what's in the list, you need
+      // to change _itemSet inside a setState call to
+      // trigger a rebuild.
+      // The framework then calls build, below,
+      // which updates the visual appearance of the app.
+
+      items.remove(item);
+      if (!completed) {
+        print("Completing");
+        _itemSet.add(item);
+        items.add(item);
+      } else {
+        print("Making Undone");
+        _itemSet.remove(item);
+        items.insert(0, item);
+      }
+    });
+  }
+
+  void _handleDeleteItem(Item item) {
+    setState(() {
+      print("Deleting item");
+      items.remove(item);
+    });
+  }
+
+  void _handleNewItem(String itemText, TextEditingController textController) {
+    setState(() {
+      print("Adding new item");
+      Item item = const Item(name: "itemText");
+      items.insert(0, item);
+      textController.clear();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('To Do List'),
         ),
-      ),
-    );
+        body: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          children: items.map((item) {
+            return ToDoListItem(
+              item: item,
+              completed: _itemSet.contains(item),
+              onListChanged: _handleListChanged,
+              onDeleteItem: _handleDeleteItem,
+            );
+          }).toList(),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (_) {
+                    return ToDoDialog(onListAdded: _handleNewItem);
+                  });
+            }));
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'To Do List',
+    home: ToDoList(),
+  ));
 }
---
+++ to_dont_list/lib/objects/item.dart
@@ -0,0 +1,11 @@
+// Data class to keep the string and have an abbreviation function
+
+class Item {
+  const Item({required this.name});
+
+  final String name;
+
+  String abbrev() {
+    return name.substring(0, 2);
+  }
+}

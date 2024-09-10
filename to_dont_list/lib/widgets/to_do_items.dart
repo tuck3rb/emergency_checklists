4381945ff8a2c3c0b6959151c09a9e0478448e89
hspayde <spaydeht@hendrix.edu>
2024-09-09 20:03:43 -0500

    Most error messages fixed.

--- to_dont_list/lib/widgets/to_do_items.dart
+++ to_dont_list/lib/widgets/to_do_items.dart
@@ -25,7 +25,7 @@ class ToDoListItem extends StatelessWidget {
     // taking place and therefore which theme to use.
 
     return completed //
-        ? Colors.black
+        ? Colors.black54
         : Theme.of(context).primaryColor;
   }
 
@@ -51,10 +51,10 @@ class ToDoListItem extends StatelessWidget {
           : null,
       leading: CircleAvatar(
         backgroundColor: _getColor(context),
-        child: Text(item.name),
+        child: Text(item.abbrev()),
       ),
       title: Text(
-        item.abbrev(),
+        item.name,
         style: _getTextStyle(context),
       ),
     );

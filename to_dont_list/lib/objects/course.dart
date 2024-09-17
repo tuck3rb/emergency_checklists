569768afbe11e9a1d9aea19546a15d9d9abe12a0
hspayde <spaydeht@hendrix.edu>
2024-09-17 10:49:56 -0500

    Added new course dialog

---
+++ to_dont_list/lib/objects/course.dart
@@ -0,0 +1,11 @@
+// Data Class to contain the Course Name & Color
+
+import 'package:flutter/material.dart';
+
+class Course {
+  const Course({required this.name, required this.color});
+
+  final String name;
+  final Color color;
+
+}
---
+++ to_dont_list/lib/widgets/new_course_dialog.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+typedef ToDoListAddedCallback = Function(
+    String value, TextEditingController textController);
+
+class ToDoDialog extends StatefulWidget {
+  const ToDoDialog({
+    super.key,
+    required this.onListAdded,
+  });
+
+  final ToDoListAddedCallback onListAdded;
+
+  @override
+  State<ToDoDialog> createState() => _ToDoDialogState();
+}
+
+class _ToDoDialogState extends State<ToDoDialog> {
+  // Dialog with text from https://www.appsdeveloperblog.com/alert-dialog-with-a-text-field-in-flutter/
+  final TextEditingController _inputController = TextEditingController();
+  final ButtonStyle yesStyle = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.green);
+  final ButtonStyle noStyle = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.red);
+
+  String valueText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Item To Add'),
+      content: TextField(
+        onChanged: (value) {
+          setState(() {
+            valueText = value;
+          });
+        },
+        controller: _inputController,
+        decoration: const InputDecoration(hintText: "type something here"),
+      ),
+      actions: <Widget>[
+        ValueListenableBuilder<TextEditingValue>(
+          valueListenable: _inputController,
+          builder: (context, value, child) {
+            return ElevatedButton(
+              key: const Key("OKButton"),
+              style: yesStyle,
+              onPressed: value.text.isNotEmpty
+                  ? () {
+                      setState(() {
+                        widget.onListAdded(valueText, _inputController);
+                        Navigator.pop(context);
+                      });
+                    }
+                  : null,
+                child: const Text('OK'),
+            );
+          },
+        ),
+        // https://stackoverflow.com/questions/52468987/how-to-turn-disabled-button-into-enabled-button-depending-on-conditions
+
+
+        ElevatedButton(
+          key: const Key("CancelButton"),
+          style: noStyle,
+          child: const Text('Cancel'),
+          onPressed: () {
+            setState(() {
+              Navigator.pop(context);
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
